f1e9f6fcaff83097dd6386644cb74ec3b5bf794a
louiebloo <leggioluke5@gmail.com>
2024-08-20 16:40:00 -0700

    life total

--- src/app/components/life-total/life-total.component.ts
+++ src/app/components/life-total/life-total.component.ts
@@ -1,13 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { bootstrapSuitHeartFill } from '@ng-icons/bootstrap-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-life-total',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent,NgClass],
   templateUrl: './life-total.component.html',
-  styleUrl: './life-total.component.css'
+  styleUrl: './life-total.component.css',
+  viewProviders: [provideIcons({ bootstrapSuitHeartFill })]
 })
 export class LifeTotalComponent {
   @Input() lifeTotal!: number;
   @Input() modifyCallback!: (amount:number)=> void;
+  @Input() editable!:boolean;
 }
