8ae79341eda1e9d699c5947c522e8a6e9d09a366
louiebloo <leggioluke5@gmail.com>
2025-02-18 18:19:02 -0800

    Tracking commander costs

--- src/app/components/property-counter/property-counter.component.ts
+++ src/app/components/property-counter/property-counter.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { IPlayer } from '../../interfaces/player';
 import { NgClass, NgIf, TitleCasePipe } from '@angular/common';
+import { TooltipDirective } from '../../directives/tooltip.directive';
 
 @Component({
   selector: 'app-property-counter',
   standalone: true,
-  imports: [NgIf,NgClass,TitleCasePipe],
+  imports: [NgIf,NgClass,TitleCasePipe,TooltipDirective],
   templateUrl: './property-counter.component.html',
   styleUrl: './property-counter.component.css'
 })
 export class PropertyCounterComponent {
   @Input() editable!:boolean;
   @Input() title!:string;
+  @Input() tooltip!:string;
   @Input() total!:number;
   // text in front of the actual number for the counter
   @Input() totalString!:string;
