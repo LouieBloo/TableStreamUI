40b96e64f58bd3f83dd14b6a1476d4c631640f7e
louiebloo <leggioluke5@gmail.com>
2025-04-08 21:56:19 -0700

    Adding reddit link, icons

--- src/app/components/pages/index/index.component.ts
+++ src/app/components/pages/index/index.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
+import { bootstrapReddit, bootstrapDiscord, bootstrapPlayFill, bootstrapInfoSquareFill } from '@ng-icons/bootstrap-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { gameAxeSword} from '@ng-icons/game-icons';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgIcon],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+  styleUrl: './index.component.css',
+  viewProviders: [provideIcons({ bootstrapReddit, bootstrapDiscord, bootstrapPlayFill, bootstrapInfoSquareFill, gameAxeSword })]
 })
 export class IndexComponent {
   constructor(private titleService: Title) {}
